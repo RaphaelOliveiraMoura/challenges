a2666c7473c5b6793992c1e243e77ba56dc1fa02
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2023-10-31 14:33:47 -0300

    feat: create map array challanges

--- challanges/004-map-filter/index.js
+++ challanges/004-map-filter/index.js
@@ -5,3 +5,19 @@ export function filterEvenNumbers(array) {
 export function filterStudents(students) {
   throw new Error("Implemente o código...");
 }
+
+export function mapStringToNumber(strings) {
+  throw new Error("Implemente o código...");
+}
+
+export function mapNumberEvenOdd(numbers) {
+  throw new Error("Implemente o código...");
+}
+
+export function mapStudentName(students) {
+  throw new Error("Implemente o código...");
+}
+
+export function mapValueTypes(values) {
+  throw new Error("Implemente o código...");
+}
--- challanges/004-map-filter/index.spec.js
+++ challanges/004-map-filter/index.spec.js
@@ -1,5 +1,12 @@
 import { expect, test, describe } from "vitest";
-import { filterEvenNumbers, filterStudents } from ".";
+import {
+  filterEvenNumbers,
+  filterStudents,
+  mapStringToNumber,
+  mapNumberEvenOdd,
+  mapStudentName,
+  mapValueTypes,
+} from ".";
 
 describe("filtrar valores", () => {
   test("deve filtrar uma lista de numeros, somente com valores pares", () => {
@@ -20,6 +27,50 @@ describe("filtrar valores", () => {
     ).toEqual([
       { nome: "jessica", nota: 8 },
       { nome: "helena", nota: 10 },
+    ]);
+  });
+});
+
+describe("mapear valores", () => {
+  test("deve retornar uma lista de strings no formato numerico", () => {
+    expect(mapStringToNumber(["5", "4", "7", "9"])).toEqual([5, 4, 7, 9]);
+    expect(mapStringToNumber([])).toEqual([]);
+    expect(mapStringToNumber(["0", "0", "0"])).toEqual([0, 0, 0]);
+    expect(mapStringToNumber(["1"])).toEqual([1]);
+  });
+
+  test("recebe uma lista de numeros, e retorna uma nova lista com as strings 'par' para numeros pares, e 'impar' para numeros impares", () => {
+    expect(mapNumberEvenOdd([4, 9, 2, 3, 4, 5])).toEqual([
+      "par",
+      "impar",
+      "par",
+      "impar",
+      "par",
+      "impar",
+    ]);
+    expect(mapNumberEvenOdd([2, 2, 2])).toEqual(["par", "par", "par"]);
+    expect(mapNumberEvenOdd([])).toEqual([]);
+  });
+
+  test("recebe uma lista de alunos e retorna somente o nome do aluno em letra maiuscula", () => {
+    expect(
+      mapStudentName([
+        { nome: "raphael", nota: 2 },
+        { nome: "jessica", nota: 8 },
+        { nome: "pedro", nota: 7 },
+        { nome: "helena", nota: 10 },
+      ])
+    ).toEqual(["RAPHAEL", "JESSICA", "PEDRO", "HELENA"]);
+  });
+
+  test("recebe uma lista com o tipo de cada valor passado na lista", () => {
+    expect(mapValueTypes(["texto", 2, {}, true, "", false])).toEqual([
+      "string",
+      "number",
+      "object",
+      "boolean",
+      "string",
+      "boolean",
     ]);
   });
 });
